fe8565d3650b03c28d639396fe746ce0291fd00b
J2mF <mp4-12cs5@hotmail.com>
2022-11-04 16:38:57 +0300

    Add rabbit mq config to notification

--- notification/src/main/java/com/amigoscode/notification/NotificationApplication.java
+++ notification/src/main/java/com/amigoscode/notification/NotificationApplication.java
@@ -1,13 +1,28 @@
 package com.amigoscode.notification;
 
+import com.amigoscode.amqp.RabbitMQMessageProducer;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.amigoscode.notification",
+                "com.amigoscode.amqp",
+        }
+)
 @EnableEurekaClient
 public class NotificationApplication {
     public static void main( String[] args ) {
         SpringApplication.run( NotificationApplication.class, args );
     }
+
+//    @Bean
+//    CommandLineRunner commandLineRunner( RabbitMQMessageProducer producer, NotificationConfig config ) {
+//        return args -> {
+//            producer.publish( "foo", config.getInternalExchange(), config.getNotificationRoutingKey() );
+//        };
+//    }
 }
