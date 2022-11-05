14781300acfecc435a769274a1aba8dd34e04736
J2mF <mp4-12cs5@hotmail.com>
2022-11-05 20:34:34 +0300

    consume notification using rabbitmq

---
+++ notification/src/main/java/com/amigoscode/notification/rabbitmq/NotificationConsumer.java
@@ -0,0 +1,21 @@
+package com.amigoscode.notification.rabbitmq;
+
+import com.amigoscode.clients.notification.NotificationRequest;
+import com.amigoscode.notification.services.NotificationService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+@Slf4j
+public class NotificationConsumer {
+    private final NotificationService notificationService;
+
+    @RabbitListener(queues = "${rabbitmq.queue.notification}")
+    public void consumer( NotificationRequest notificationRequest ) {
+        log.info( "Consumed {} from queue", notificationRequest );
+        notificationService.send( notificationRequest );
+    }
+}
